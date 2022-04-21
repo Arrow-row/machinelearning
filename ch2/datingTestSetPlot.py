0171df94aa3e1916dccd2416d3618dc4ea1cc9d7
Arrow <1512803831@qq.com>
2022-04-21 17:55:53 +0800

    'plot'

---
+++ ch2/datingTestSetPlot.py
@@ -0,0 +1,37 @@
+from numpy import *
+import kNN
+import matplotlib
+import matplotlib.pyplot as plt
+
+'''
+datingTestSet.txt 中为样本数据集，前3列为样本特征，第4列为类型标签
+第1列:每年获得的飞行常客里程数
+第2列:玩视频游戏所耗时间百分比
+第3列:每周消费的冰淇淋公升数
+第4列:类型标签，包括不喜欢、一般、很喜欢3类
+'''
+datingDataMat,datingLabels=kNN.file2matrix('datingTestSet.txt') 
+fig=plt.figure()
+ax=fig.add_subplot(111)
+
+'''
+#fig_2-3
+ax.scatter(datingDataMat[:,1],datingDataMat[:,2])  
+plt.xlabel('Percentage of Time Spent Playing Video Games')
+plt.ylabel('Liters of Ice Cream Consumed Per Week')
+'''
+
+'''
+#fig_2-4
+ax.scatter(datingDataMat[:,1],datingDataMat[:,2],15.0*array(datingLabels),15.0*array(datingLabels))  
+plt.xlabel('Percentage of Time Spent Playing Video Games')
+plt.ylabel('Liters of Ice Cream Consumed Per Week')
+'''
+
+
+#fig_2-5
+ax.scatter(datingDataMat[:,0],datingDataMat[:,1],15.0*array(datingLabels),15.0*array(datingLabels))  
+plt.xlabel('Frequent Flyier Miles Earned Per Year')
+plt.ylabel('Percentage of Time Spent Playing Video Games')
+plt.show()
+
